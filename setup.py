024c7aba8377498e96c633ffafd07126444ff1c7
Michele Castriotta <mich.castriotta@gmail.com>
2025-12-27 12:11:20 +0100

    Force numpy<2.4 to avoid uv runtime error on numba==0.53

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 install_requires = [
     "jsonschema",
-    "numpy",
+    "numpy<2.4",
     "opencv-python-headless",
     "pillow",
     "pooch",
